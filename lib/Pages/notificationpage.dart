821ea597a34785513a753516030f301ed17e8ab2
Nabil Fadly Azhar Aziz <nabilfadly28@gmail.com>
2025-01-08 14:40:11 +0700

    navigation

---
+++ lib/Pages/notificationpage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Notificationpage extends StatelessWidget {
+  const Notificationpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('notification page'),
+    );
+  }
+}
---
+++ lib/Widgets/navbar.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mejakerja/Pages/HomePages/homepage.dart';
+import 'package:mejakerja/Pages/ProfilePages/profilepage.dart';
+import 'package:mejakerja/Pages/notificationpage.dart';
+import 'package:mejakerja/Pages/postpage.dart';
+import 'package:mejakerja/Pages/searchpage.dart';
+
+class Navbar extends StatefulWidget {
+  const Navbar({Key? key}) : super(key: key);
+
+  @override
+  _Navbar createState() => _Navbar
+  ();
+}
+
+class _Navbar extends State<Navbar> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    const Homepage(),
+    Searchpage(),
+    Postpage(),
+    Notificationpage(),
+    Profilepage()
+
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search_outlined),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Post',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications_none_outlined),
+            label: 'Notification',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
